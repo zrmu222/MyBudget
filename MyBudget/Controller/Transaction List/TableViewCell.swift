a7020e77ead69ae29064d72b02653c05e6655946
Zack Murphy <zrm725@yahoo.com>
2017-11-24 12:22:45 -0500

    First Commit

---
+++ MyBudget/Controller/Transaction List/TableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  TableViewCell.swift
+//  MyBudget
+//
+//  Created by Zachary Murphy on 11/13/17.
+//  Copyright © 2017 Zachary Murphy. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    // MARK: - Public Properties
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    var transaction: Transaction! {
+        didSet{
+            updateView()
+        }
+    }
+    
+    // MARK: - Private Properties
+    
+    // MARK: - <#superclass#>
+    
+    // MARK: - Public Methods
+    
+    // MARK: - Private Methods
+    private func updateView(){
+        nameLabel.text = transaction.name!
+        priceLabel.text = transaction.price!
+    }
+    
+    // MARK: - <#delegate#>
+    
+    // MARK: - Actions
+
+}
